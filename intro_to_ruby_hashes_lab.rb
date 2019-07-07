3194fb98e8f439e63c173d9547732fd35392a0fd
Victoria Cushing <victoriaccushing@gmail.com>
2019-07-07 03:29:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,10 @@ def new_hash(new_hash = {})
 end
 
 def my_hash(my_hash)
-  my_hash
+  my_hash {
+    :message = "Hello World"
+    
+  }
   # return a valid hash with any key/value pair of your choice
 end
 
