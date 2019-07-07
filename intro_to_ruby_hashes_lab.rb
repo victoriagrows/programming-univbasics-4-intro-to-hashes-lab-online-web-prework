6423b4d88be1d36bde1e8f4f544e76e05101a4c4
Victoria Cushing <victoriaccushing@gmail.com>
2019-07-07 03:36:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash(new_hash = {})
   new_hash
 end
 
-def my_hash(pets = {})
+def my_hash(pets)
   pets = {:cat => "Maru", :dog => "Pluto"}
   # return a valid hash with any key/value pair of your choice
 end
